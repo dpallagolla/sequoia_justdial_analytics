01c46c7bfdab1dc2b521298b43eb4fd86e1f6314
dwarakesh <dwarakesh@devmachine.(none)>
2014-09-20 23:01:16 +0530

    Unfinished Fetch category details

--- main.py
+++ main.py
@@ -2,26 +2,31 @@
 import cgi
 import datetime
 import webapp2
+import logging
 
 from google.appengine.ext import ndb
 from google.appengine.api import users
 from google.appengine.api import urlfetch
 import json
 
-guestbook_key = ndb.Key('Guestbook', 'default_guestbook')
 
-class Greeting(ndb.Model):
-  author = ndb.UserProperty()
-  content = ndb.TextProperty()
-  date = ndb.DateTimeProperty(auto_now_add=True)
+class categories(ndb.Model):
+  category = ndb.StringProperty()
+  
+class categoryDetails(ndb.Model):
+    category = ndb.StringProperty()
+    name = ndb.StringProperty()
+    companyGeocodes = ndb.StringProperty()
+    address = ndb.StringProperty()
 
 
-class MainPage(webapp2.RequestHandler):
+class FetchCategories(webapp2.RequestHandler):
   def get(self):
     result = ""
     url1 = "http://hack2014.justdial.com/search/json/completedefault/%s/13043647/77620617/100km/10"
     cat_list = []
 
+    #get categories from aa-zz and put them in cat_list
     for x in xrange(ord('a'), ord('z')+1):
       for y in xrange(ord('a'), ord('z')+1):
         url = url1 % (chr(x)+chr(y))
@@ -54,6 +59,56 @@ class MainPage(webapp2.RequestHandler):
         result += tmp + "\n"
     self.response.out.write(result)
 
+    #write data to db
+    for categ in cat_list:
+        c = categories()
+        c.category = categ
+        c.put()
+        logging.info('%s added to DB' % categ)
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.out.write('Under construction')
+
+class FetchCategoryDetails(webapp2.RequestHandler):
+    def get(self):
+        url = "http://hack2014.justdial.com/search/json/justdialapicat/%s/bangalore/bangalore/13043647/77620617/150km/100/%d"
+        categories = ndb.gql('SELECT * FROM categories')
+        for category in categories:
+            docid_list = []
+            for i in range(1,10):
+                url = url % (category,i)
+                try:
+                rdata = urlfetch.fetch( url = url, 
+                                    follow_redirects = False, 
+                                    deadline = 30)
+                except Exception as e:
+                    log = "HTTP error"
+                    logging.warning(log)
+                    log = traceback.format_exc()
+                    logging.warning(log)
+                    # code HTTP error
+                    code = {"code" : 1, "desc":"HTTP error - " + str(e)}
+                    final_json = json.dumps(code)
+                    self.response.out.write(final_json)
+                    return
+
+                if rdata.status_code == 200:
+                    data = json.loads(rdata.content)
+                    for res in data["results"]:
+                        if res not in docid_list:
+                            docid_list.append(res["docId"])
+                            # TODO add to db
+                            c = categoryDetails()
+                            c.category = category
+                            c.name = res["name"]
+                            c.address = res["companyGeocodes"]
+                            c.put()
 
 
-app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
+                        
+    self.response.headers['Content-Type'] = 'text/plain'    
+
+
+
+app = webapp2.WSGIApplication([('/fetchCategories', FetchCategories),('/',MainPage),('/fetchCategoryDetails',MainPage)], debug=True)
